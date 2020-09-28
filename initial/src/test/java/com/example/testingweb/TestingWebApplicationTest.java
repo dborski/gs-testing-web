06833d2c41031a15141582f10e8a7a7beccd95cb
Derek Borski <derek.borski@gmail.com>
2020-09-28 13:14:14 -0600

    Add spec for testing that web application is running properly

---
+++ initial/src/test/java/com/example/testingweb/TestingWebApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.testingweb;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTest {
+
+  @Test
+  public void contextLoads() {
+  }
+  
+}
